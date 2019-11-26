74f243272c7abc538b2959ece5a085f3d66b8bba
Jurgen <info@jold.nl>
2019-11-27 00:10:51 +0100

    Add default options, and add rel or target attributes to the <a> element

--- jquery.jold.external-hrefs.js
+++ jquery.jold.external-hrefs.js
@@ -20,38 +20,95 @@
          * Pick up the options passed to the plugin
          */
         var settings = $.extend({
-            param: 'defaultValue'
+            'set_target': false,
+            'set_rel': false,
+            'ignore_www': false,
         }, options || {});
-
 
         $element.each(function() {
 
-            /** Create url object from link */
+            // Create url object from link
             var link = new URL( this.href );
+
+            // Current element jQuery object
+            $elem = $(this);
 
             /**
              * Link href host is diffrent from current page url hostname
              */
             if( link.host != hostname ) {
 
-                $(this).on('click', function(event) {
+                // Dont do anything if target="_self"
+                if ($elem.attr('target') !== '_self') {
 
-                    event.preventDefault();
-                    event.stopPropagation();
-
-                    /**
-                     * Only open http links in new tab, skip all other protocols (tel, mailto)
-                     */
-                    if (link.protocol == 'http:' || link.protocol == 'https:') {
-                        window.open(this.href, '_blank');
-                    } else {
-                        window.location.replace(this.href);
+                    // Set target attribute if setting is true
+                    if (settings.set_target == true) {
+                        setTargetAttribute( $elem );
                     }
 
-                });
+                    // Set rel attribute if setting is true
+                    if (settings.set_rel == true) {
+                        setRelAttribute( $elem );
+                    }
+
+                    // Register click event on <a> and open external link in new tab
+                    $(this).on('click', function(event) {
+
+                        event.preventDefault();
+                        event.stopPropagation();
+
+                        /**
+                         * Only open http links in new tab, skip all other protocols (tel, mailto)
+                         */
+                        if (link.protocol == 'http:' || link.protocol == 'https:') {
+                            window.open(this.href, '_blank');
+                        } else {
+                            window.location.replace(this.href);
+                        }
+
+                    });
+
+                }
+
             }
 
         });
+
+
+        /**
+         * Check if element attribute exists
+         * @param   object    $elem  jQuery element
+         * @param   string    attr   Attribute to check
+         * @return  boolean          False|True if attributes exists on the element
+         */
+        function checkElemAttr($elem, attr) {
+            var attribute = $elem.attr(attr);
+            if (typeof attribute !== typeof undefined && attribute !== false) {
+                return true;
+            }
+            return false;
+        }
+
+        /**
+         * Set rel attibute on element if not yet present
+         * @param  object  $elem  jQuery element
+         */
+        function setRelAttribute($elem) {
+            if (!checkElemAttr($elem, 'rel') ) {
+                // if ($elem.attr('target') !== '_self') {}
+                $elem.attr('rel', 'external')
+            }
+        };
+
+        /**
+         * Set target attibute on element if not yet present
+         * @param  object  $elem  jQuery element
+         */
+        function setTargetAttribute($elem) {
+            if (!checkElemAttr($elem, 'target') ) {
+                $elem.attr('target', '_blank')
+            }
+        };
 
     };
 
